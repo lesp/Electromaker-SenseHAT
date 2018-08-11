55e1737c8f8edd64e7964eefa43a6a93d4a2c5d1
Les Pounder <lespounder@gmail.com>
2018-08-11 16:19:55 +0000

    Added an except to claer the screen

--- Rainbow.py
+++ Rainbow.py
@@ -5,24 +5,27 @@ sense = SenseHat()
 sense.clear()
 
 
-
-while True:
-    o = sense.get_orientation()
-    pitch = o["pitch"]
-    roll = o["roll"]
-    yaw = o["yaw"]
-    print("pitch {0} roll {1} yaw {2}".format(pitch, roll, yaw))
-    if pitch > 254:
-        pitch = 254
-    if pitch < 1:
-        pitch = 0
-    if roll > 254:
-        roll = 254
-    if roll < 1:
-        roll = 0
-    if yaw > 254:
-        yaw = 254
-    if yaw < 1:
-        yaw = 0
-    sense.clear(int(pitch),int(roll),int(yaw))
-    sleep(0.1)
+try:
+    while True:
+        o = sense.get_orientation()
+        pitch = o["pitch"]
+        roll = o["roll"]
+        yaw = o["yaw"]
+        print("pitch {0} roll {1} yaw {2}".format(pitch, roll, yaw))
+        if pitch > 254:
+            pitch = 254
+        if pitch < 1:
+            pitch = 0
+        if roll > 254:
+            roll = 254
+        if roll < 1:
+            roll = 0
+        if yaw > 254:
+            yaw = 254
+        if yaw < 1:
+            yaw = 0
+        sense.clear(int(pitch),int(roll),int(yaw))
+        sleep(0.1)
+except KeyboardInterrupt:
+    sense.clear()
+    
